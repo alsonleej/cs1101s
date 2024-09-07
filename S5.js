a614e86faa7f7cbc11310181d66ef5a2d2d0de64
Source Academy User <No public email provided>
2024-09-08 01:01:42 +0800

    Changes made from Source Academy

---
+++ S5.js
@@ -0,0 +1,33 @@
+//S5
+
+//2
+function every_second(items) {
+    return is_null(items)
+        ? items
+        : is_null(tail(items)) 
+        ? null
+        : pair(head(tail(items)), every_second(tail(tail(items))));
+}
+
+every_second(list("a", "x", "b", "y", "c", "z", "d"));
+
+// Value: ["x", ["y", ["z", null]]]
+
+//3
+
+const lst1 = list(7, list(6, 5, 4), 3, list(2, 1));
+
+head(tail(head(tail(tail(tail(lst1))))));
+
+const lst2 = list(list(7), list(6, 5, 4), list(3, 2), 1);
+
+head(tail(tail(tail(lst2))));
+
+const lst3 = list(7, list(6), list(5, list(4)), list(3, list(2, list(1))));
+
+head(head(tail(head(tail(head(tail(tail(tail(lst3)))))))));
+
+const lst4 = list(7, list(list(6, 5), list(4), 3, 2), list(list(1)));
+
+head(head(head(tail(tail(lst4)))));
+
