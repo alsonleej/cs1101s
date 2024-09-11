d026420ce79f9573fd13ea43a935885b61c8f701
Source Academy User <No public email provided>
2024-09-12 03:37:50 +0800

    Changes made from Source Academy

---
+++ R5.js
@@ -0,0 +1,31 @@
+//R5
+
+//1
+
+function flatten_list(lst) {
+    return is_null(lst)
+        ? null 
+        : is_number(lst)
+        ? list(lst)
+        : accumulate((x, y) => append(flatten_list(x), y), null, lst);
+}
+const LoL = list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8, 9));
+flatten_list(LoL);
+// Returns list(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+//2
+
+function tree_sum(tree) {
+    return is_null(tree)
+        ? 0 
+        : is_number(tree)
+        ? tree
+        : accumulate((x, y) => tree_sum(x) + y, 0, tree);
+}
+
+
+const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
+tree_sum(my_tree);
+// Returns 28
+
+//3
