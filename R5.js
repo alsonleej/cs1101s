996efbab0dacc188bbf7e237293ca8c96a5fb690
Source Academy User <No public email provided>
2024-09-12 10:42:01 +0800

    Changes made from Source Academy

--- R5.js
+++ R5.js
@@ -1,14 +1,26 @@
 //R5
 
+// for reference
+function accumulate(op, initial, sequence) {
+    return is_null(sequence)
+           ? initial
+           : op(head(sequence), 
+                accumulate(op, initial, tail(sequence)));
+}
 //1
 
 function flatten_list(lst) {
     return is_null(lst)
-        ? null 
+        ? null // unnecessary! it is already perfored in accumulate
         : is_number(lst)
         ? list(lst)
         : accumulate((x, y) => append(flatten_list(x), y), null, lst);
 }
+//This is a much better solution!!
+function flatten_list(lst) {
+    return accumulate(append, null, lst);
+}
+
 const LoL = list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8, 9));
 flatten_list(LoL);
 // Returns list(1, 2, 3, 4, 5, 6, 7, 8, 9)
@@ -22,7 +34,15 @@ function tree_sum(tree) {
         ? tree
         : accumulate((x, y) => tree_sum(x) + y, 0, tree);
 }
-
+//This is alternative solution!!
+function tree_sum(tree) {
+    return is_null(tree)
+        ? 0 
+        : ( is_list(head(tree))
+            ? tree_sum(head(tree))
+            : head(tree))
+        + tree_sum(tail(tree));
+}
 
 const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
 tree_sum(my_tree);
@@ -30,11 +50,11 @@ tree_sum(my_tree);
 
 //3
 function accumulate_tree(f, op, initial, tree) {
-    return accumulate( is_null(tree) 
-                    ? initial
-                    : is_number(tree)
-                    ? f(tree)
-                    : (x, y) => op(accumulate_tree(f, op, initial, x), y), 
+    return accumulate( 
+                     (x, y) => 
+                            is_number(x)
+                            ? op(f(x), y)
+                            : op(accumulate_tree(f, op, initial, x), y), 
                         initial, tree);
 }
 
@@ -43,3 +63,5 @@ function tree_sum2(tree) {
 }
 
 tree_sum2(my_tree);
+
+// op needs to be commutative and associative. initial is a neutral element wrt to op
