ec6a05b80797f80c7bf77f06cd65a07690c4df9b
Source Academy User <No public email provided>
2024-09-16 18:11:10 +0800

    Changes made from Source Academy

--- S6.js
+++ S6.js
@@ -55,3 +55,41 @@ display_list(makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50)));
 // Result: list(list(20, 1, 1), list(10, 5, 1, 5, 1), list(1, 20, 1),
 // list(1, 20, 1), list(1, 10, 5, 5, 1),
 // list(1, 10, 5, 1, 5))
+
+//in-class
+//1: same as above
+//2
+
+function subsets(xs) {
+    if (is_null(xs)) {
+        return list(null);
+    } else {
+        // Combinations that do not use the head element.
+        const combi_A = subsets(tail(xs));
+        // // Combinations that do not use the head coin
+        // // for the remaining amount.
+        // const combi_B = makeup_amount(x - head(coins), tail(coins));
+        // Combinations that use the head element.
+        const combi_C = map(x => pair(head(xs), x), combi_A);
+        return append(combi_A, combi_C);
+    }
+}
+subsets(list(1, 2, 3));
+
+// recurrence relation T(n) = 2T(n-1) + O(1)
+// T(n-1) from subsets(tail(xs));, T(n-1) from map(x => ..., subsets(tail(xs)), 
+// order of growth O(2^n)
+
+//3
+function permutations(s) {
+    return is_null(s)
+        ? list(null)
+        : accumulate(append, null,
+            map(x => map(p => pair(x, p),
+                            permutations(remove(x, s))),
+    s));
+}
+
+// recurrence relation T(n) = n*T(n-1) 
+// n from map(x => ..., s), T(n-1) from permutations 
+// order of growth O(n!)
