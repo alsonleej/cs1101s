4bfbc3c5987b8d3b527fa37222bd68f70f7068b8
Source Academy User <No public email provided>
2024-10-14 11:36:45 +0800

    Changes made from Source Academy

---
+++ S9ic.js
@@ -0,0 +1,66 @@
+//S9ic
+//C:/Users/alson/Downloads/studio-S9-in-class.pdf
+function count_pairs(x) {
+    if (!is_pair(x)) {
+        return 0;
+     } else {
+        return 1 + count_pairs(head(x)) + count_pairs(tail(x));
+     }
+}
+
+
+// Show that this function is not correct. In particular, draw box-and-pointer diagrams
+// representing list structures made up of exactly three pairs for which Ben’s procedure would
+// return 3, return 4, return 7, or never return at all.
+
+// his function is incorrect because head and tail can point to the same list.
+
+// success case, where his program works
+const lst1 = list(1,2,3);
+draw_data(lst1);
+display(count_pairs(lst1));
+
+//3
+const k = pair(null,null);
+const lst3= pair(k,k);
+draw_data(lst3);
+display(count_pairs(lst3));
+
+//4
+const lst4 = pair(lst3, null);
+draw_data(lst4);
+display(count_pairs(lst4));
+
+//7
+const lst7 = pair(lst3, lst3);
+draw_data(lst7);
+display(count_pairs(lst7));
+
+//infinite
+const lsti = list(1,2,3);
+set_tail(tail(tail(lsti)), lsti);
+draw_data(lsti);
+// display(count_pairs(lsti)); //infinite loop
+
+// correct implementation
+function countpairs(x){
+    let pairs_seen = null;
+    function check(x){
+        if (is_null(x)){
+            return 1;
+        }
+        else if (is_null(member(head(x)), pairs_seen)){
+            pairs_seen = append(list(head(x)), pairs_seen);
+        } else {
+            check(head(x));
+            check(tail(x));
+        }
+    }
+    return length(pairs_seen);
+}
+
+display(countpairs(lst1));
+display(countpairs(lst3));
+display(countpairs(lst4));
+display(countpairs(lst7));
+
