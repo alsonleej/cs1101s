9d6665f6c89792296c8adea0419538ea9c719bcf
Source Academy User <No public email provided>
2024-11-08 22:01:24 +0800

    Changes made from Source Academy

---
+++ array_processing.js
@@ -0,0 +1,55 @@
+function array_join(arr){
+    //flattens all nested arrays
+    let k = 0;
+    let glo = [];
+    
+    function hjoin(arr){
+        const l = array_length(arr);
+        if (!is_array(arr)){
+            glo[k] = arr;
+            k = k + 1;
+        }
+        else {
+            for (let idx = 0; idx < l; idx =  idx + 1){
+                hjoin(arr[idx]);
+            }
+        }
+    }
+    
+    hjoin(arr);
+    return glo;
+    
+}
+
+function array_slice(arr, start, end){
+    if (!is_array(arr)){
+        return arr;
+    }
+    else if (start === end){
+        return [];
+    }
+    else if (start > end){
+        error(start, "after end");
+    }
+    else {
+        const l = array_length(arr);
+        if (end > l){
+            error(end, "beyond array index");
+        }
+        const copy = [];
+        for (let i = start; i < end; i = i + 1){
+            copy[i - start] = array_copy(arr[i]);
+        }
+        return copy;
+    }
+}
+
+function array_copy(arr){
+    const k = array_length(arr);
+    return array_slice(arr, 0, k);
+}
+
+function array_pop(arr){
+    const k = array_length(arr);
+    return array_slice(arr, 0, k - 1);
+}
